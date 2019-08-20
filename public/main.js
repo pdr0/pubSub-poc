a0c9f01ce80e1a3abbdc504d292398439133e8a4
pdr0 <me@pedroselas.com>
2019-08-20 11:23:05 +0200

    Add plubic JS files to show the Demo

---
+++ public/main.js
@@ -0,0 +1,49 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+var _topics = _interopRequireDefault(require("../topics"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var onMessage = function onMessage(msg, data) {
+  return console.log("TOPIC: ".concat(msg, " - payload: ").concat(data));
+};
+
+var loginButton = document.querySelector('.login');
+var helpButton = document.querySelector('.help');
+var searchButton = document.querySelector('.search');
+window.messages.subscribe(_topics["default"]['lotto.container-1.click'], onMessage);
+window.messages.subscribe(_topics["default"]['lotto.container-2.click'], onMessage);
+window.messages.subscribe(_topics["default"]['lotto.container-3.click'], onMessage);
+window.messages.subscribe(_topics["default"]['lotto.container-4.click'], onMessage);
+loginButton.addEventListener('click', function (evt) {
+  window.messages.publish(_topics["default"]["lotto.main.login.click"], true);
+});
+helpButton.addEventListener('click', function (evt) {
+  window.messages.publish(_topics["default"]["lotto.main.help.click"], true);
+});
+searchButton.addEventListener('click', function (evt) {
+  window.messages.publish(_topics["default"]["lotto.main.search.click"], true);
+});
+window.messages.publishSync(_topics["default"]['lotto.main.loaded'], true);
+},{"../topics":2}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  'lotto.main.header.expanded': 'lotto.main.header.expanded',
+  'lotto.container-1.click': 'lotto.container-1.click',
+  'lotto.container-2.click': 'lotto.container-2.click',
+  'lotto.container-3.click': 'lotto.container-3.click',
+  'lotto.container-4.click': 'lotto.container-4.click',
+  'lotto.main.loaded': 'lotto.main.loaded',
+  'lotto.container-1.loaded': 'lotto.container-1.loaded',
+  'lotto.main.search.click': 'lotto.main.search.click',
+  'lotto.main.help.click': 'lotto.main.help.click',
+  'lotto.main.login.click': 'lotto.main.login.click'
+};
+exports["default"] = _default;
+},{}]},{},[1]);
